110f5f31af1bf97173a35d0b5f1e36498044b8df
Neeraj Bhengra <37010429+bhengraneeraj@users.noreply.github.com>
2022-03-21 16:13:10 +0530

    Add files via upload

---
+++ RockPaperScissor.py
@@ -0,0 +1,23 @@
+import random
+
+def play():
+    user=input("'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer=random.choice(['r','p','s'])
+
+    ## rules-  r>s p>r s>p
+    if user==computer:
+        return 'Tie'
+    
+    if win(user,computer):
+        print(f'You Win.Computer\'s choice: {computer}')
+    else :
+      return(f'You Lost.Computer\'s choice: {computer}')
+     
+
+
+def win(player,opponent):
+        if (player=='r' and opponent=='s') or (player=='p' and opponent=='r') or (player=='s' and opponent=='p'):
+            return True
+    
+print(play())
+
